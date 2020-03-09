2382b6a01789df4d39c1b597851ea57b67998abe
elipmoc <relipmoc101@gmail.com>
2020-03-09 15:33:26 +0900

    [feat]直和型のパターンマッチ風の何かができるマクロをサポート

---
+++ TeaGameLib/include/Variant.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <variant>
+
+#define match(x) std::visit([&](auto&& x)
+#define case(x,t) if constexpr (std::is_same_v<std::decay_t<decltype(x)>, t>)
+#define case_expr(x,t) case(x,t) return
+#define else_expr else return
+#define match_end(x)  ,x )
+
+namespace teaGameLib {
+	template<typename ...Args>
+	using Variant = std::variant<Args...>;
+}
+
