c1b8bfba1d58bf2b962754f0943323691b9093f7
elipmoc <relipmoc101@gmail.com>
2020-03-09 23:51:20 +0900

    [refactor]visitの引数入れ替え黒魔術をシンプルなものに

--- TeaGameLib/include/Variant.hpp
+++ TeaGameLib/include/Variant.hpp
@@ -2,8 +2,8 @@
 #include <variant>
 #include <tuple>
 
-#define match(x) teaGameLib::Visit(x,[&](auto&& x)
-#define match2(x1,x2) teaGameLib::Visit(x1,x2,[&](auto&& x1,auto&& x2)
+#define match(x) teaGameLib::visit(x,[&](auto&& x)
+#define match2(x1,x2) teaGameLib::visit(x1,x2,[&](auto&& x1,auto&& x2)
 #define ret_match(x) return match(x)
 #define ret_match2(x1,x2) return match2(x1,x2)
 #define case(x,t) if constexpr (std::is_same_v<std::decay_t<decltype(x)>, t>)
@@ -17,40 +17,24 @@
 
 namespace teaGameLib {
 
-	template<typename Tuple, std::size_t ...I>
-	struct tuple_pick {
-		tuple_pick(Tuple, std::index_sequence<I...>) {}
-		using Type = std::tuple< std::tuple_element_t<I, Tuple>...>;
-	};
-
-	template<typename Callable, typename ...Variants>
-	struct visit_helper {
-		visit_helper(std::tuple<Callable, Variants...>) {};
-		static auto Visit(Variants&& ...variants, Callable&& callable) {
-			return std::visit(std::forward<Callable>(callable), std::forward<Variants>(variants)...);
-		}
-	};
-
 	template<typename ...Args>
 	using Variant = std::variant<Args...>;
 
-	template<typename ...Args>
-	auto Visit(Args&& ...args){
-		using ArgsTuple = std::tuple<Args...>;
-		using ArgsSize = std::tuple_size<ArgsTuple>;
-		using HeadsIndexSeq = std::make_index_sequence<ArgsSize::value - 1>;
-		using TailIndexSeq = std::index_sequence<ArgsSize::value - 1>;
-		using HeadsTuple = typename decltype(
-			tuple_pick{ std::declval<ArgsTuple>(),HeadsIndexSeq{} }
-		)::Type;
-		using TailTuple = typename decltype(
-			tuple_pick{ std::declval<ArgsTuple>(),TailIndexSeq{} }
-		)::Type;
-
-		using LastTuple = decltype(
-			std::tuple_cat(std::declval<TailTuple>(), std::declval<HeadsTuple>())
+	template<typename ...Args, std::size_t ...I>
+	decltype(auto) visit_impl(std::index_sequence<I...>, Args&&... args) {
+		auto argTuple = std::forward_as_tuple(std::forward<Args>(args)...);
+		return std::visit(
+			std::get<sizeof...(I)>(argTuple),
+			std::get<I>(argTuple)...
 		);
-		return decltype(visit_helper{ std::declval<LastTuple>() })::Visit(std::forward<Args>(args)...);
+	}
+
+	template<typename ...Args>
+	decltype(auto) visit(Args&& ...args) {
+		return visit_impl(
+			std::make_index_sequence<sizeof...(Args) - 1>{},
+			std::forward<Args>(args)...
+		);
 	}
 }
 
